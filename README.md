cd359d0bcbdd9d6ae656db1142268d66dd00adce
Neill Robson <neill@neillrobson.com>
2026-01-05 13:36:33 -0500

    Demonstrate bytestring loading

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Advent of Code in Haskell
 
 > In which Neill learns Haskell via Advent of Code.
+
+## Setup
+
+For the Advent of Code API to work, a session key needs to be stored in `/aoc-conf.yaml`.
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,25 @@
 module Main (main) where
 
+import Control.Exception
+import Control.Monad
+import qualified Data.ByteString as BS
+import System.IO.Error (isDoesNotExistError)
+
+-- data Config = Cfg { _cfgSession :: Maybe String }
+
+defaultConfPath :: FilePath
+defaultConfPath = "aoc-conf.yaml"
+
+-- configFile :: FilePath -> IO Config
+-- configFile fp = do
+--   cfgInp <- tryJust (guard . isDoesNotExistError) $ BS.readFile fp
+--   return cfgInp
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  cfgInp <- tryJust (guard . isDoesNotExistError) $ BS.readFile defaultConfPath
+  case cfgInp of
+    Left () ->
+      putStrLn "File does not exist"
+    Right b ->
+      BS.putStr b
