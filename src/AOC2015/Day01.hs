8fae496f515e8cc132c511b6ac923dc97e2d6ce4
Neill Robson <neill@neillrobson.com>
2026-01-16 09:35:53 -0500

    complete 2015 day 1 part 1

---
+++ src/AOC2015/Day01.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE ViewPatterns #-}
+
+module AOC2015.Day01 (run) where
+
+import Control.Applicative (Alternative (empty))
+import Control.DeepSeq (force)
+import Control.Exception (evaluate, tryJust)
+import Control.Monad (guard)
+import qualified Data.Text as T
+import System.IO.Error (isDoesNotExistError)
+
+--------------------------------------------------------------------------------
+
+eitherToMaybe :: (Alternative m) => Either e a -> m a
+eitherToMaybe = either (const empty) pure
+
+strip :: String -> String
+strip = T.unpack . T.strip . T.pack
+
+stripNewline :: String -> String
+stripNewline = reverse . dropWhile (== '\n') . reverse
+
+--------------------------------------------------------------------------------
+
+inputFilePath :: FilePath
+inputFilePath = "data/2015/01.txt"
+
+readFileMaybe :: FilePath -> IO (Maybe String)
+readFileMaybe = (traverse (evaluate . force) . eitherToMaybe =<<) . tryJust (guard . isDoesNotExistError) . readFile
+
+--------------------------------------------------------------------------------
+
+level :: String -> Integer
+level = foldl' go 0
+  where
+    go i c
+      | c == '(' = i + 1
+      | c == ')' = i - 1
+      | otherwise = undefined
+
+run :: IO ()
+run = do
+  input <- readFileMaybe inputFilePath
+  case input of
+    Nothing -> putStrLn "cannot read input"
+    Just (stripNewline -> inp) -> do
+      putStrLn inp
+      putStrLn $ show $ level inp
