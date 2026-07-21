a87329a3beb9d3ff84d8ab40bb7b7ebeccacf751
Neill Robson <neill@neillrobson.com>
2026-07-21 13:27:50 -0400

    All fixed bodies

--- src/Sandbox/Templates.hs
+++ src/Sandbox/Templates.hs
@@ -4,6 +4,9 @@ import Language.Haskell.TH
 
 -- Source: https://www.parsonsmatt.org/2015/11/15/template_haskell.html
 
+bigBadMathProblem :: Int -> Double
+bigBadMathProblem = (2 /) . fromIntegral
+
 {-
 Desired output of precompute:
 
@@ -15,8 +18,16 @@ lookupTable x = bigBadMathProblem x
 Each Int in our input list should be one pattern in the output.
 -}
 
+intToPat :: Int -> Pat
+intToPat = LitP . IntegerL . toInteger
+
+precomputeInteger :: Int -> Exp
+precomputeInteger = LitE . DoublePrimL . toRational . bigBadMathProblem
+
 precompute :: [Int] -> DecsQ
 precompute xs = do
   let name = mkName "lookupTable"
-  let clauses = undefined
+  let patterns = map intToPat xs
+  let bodies = map precomputeInteger xs
+  let clauses = zipWith (\pat body -> Clause [pat] (NormalB body) []) patterns bodies
   return [FunD name clauses]
