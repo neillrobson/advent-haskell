750da1d16844f18362811e9d806ea3b428a600a2
Neill Robson <neill@neillrobson.com>
2026-07-21 12:43:17 -0400

    template name

--- src/Sandbox/Templates.hs
+++ src/Sandbox/Templates.hs
@@ -4,9 +4,19 @@ import Language.Haskell.TH
 
 -- Source: https://www.parsonsmatt.org/2015/11/15/template_haskell.html
 
+{-
+Desired output of precompute:
+
+lookupTable 0 = 123.456
+lookupTable 12 = 151626.4234
+...
+lookupTable x = bigBadMathProblem x
+
+Each Int in our input list should be one pattern in the output.
+-}
+
 precompute :: [Int] -> DecsQ
 precompute xs = do
-  -- fill me in
-  let name = undefined
+  let name = mkName "lookupTable"
   let clauses = undefined
   return [FunD name clauses]
